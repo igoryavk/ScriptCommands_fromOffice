87869437495360ef3171cccfd15f57d85dfa1f80
igoryavk <last85@list.ru>
2024-01-09 16:51:23 +0300

    Удалено все лишнее. Добавлены файлы assemble.py и disasseble.py 	new file:   assemble.py 	deleted:    command.py 	modified:   config.json 	new file:   disassemble.py 	deleted:    export.py 	deleted:    icon.ico 	deleted:    icon2.ico 	deleted:    import.ico 	deleted:    import.py 	deleted:    importPlcOpen.ico 	deleted:    importPlcOpen.py 	deleted:    list_objects.py 	deleted:    python.ico 	deleted:    pyton.py 	deleted:    results.xml 	deleted:    start.bat 	deleted:    "test \342\200\224 \320\272\320\276\320\277\320\270\321\217.py" 	deleted:    test.py

---
+++ assemble.py
@@ -0,0 +1,4 @@
+from os import system as load
+
+
+load("python C://Users//i.yavkin//PycharmProjects//OpnPlcParser//create_tree.py")
---
+++ disassemble.py
@@ -0,0 +1,4 @@
+from os import system as load
+
+
+load("python C://Users//i.yavkin//PycharmProjects//OpnPlcParser//disassemble_tree.py")
